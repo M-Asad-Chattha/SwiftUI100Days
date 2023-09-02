7e1f6d365141872e07ee2eecd1298ebc18c25d71
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-09-02 11:55:22 +0500

    Initial Commit

---
+++ SwiftUI100Days/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI100Days
+//
+//  Created by Muhammad Asad Chattha on 02/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI100Days/SwiftUI100DaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100DaysApp.swift
+//  SwiftUI100Days
+//
+//  Created by Muhammad Asad Chattha on 02/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100DaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
