1b86f3a35e5d7c7b45627e33b08c9b600dd0e545
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-09-05 23:34:24 +0500

    Linear Gradient with Gradient Stops

--- SwiftUI100Days/Project-2/ContentViewProject_2.swift
+++ SwiftUI100Days/Project-2/ContentViewProject_2.swift
@@ -10,7 +10,18 @@ import SwiftUI
 struct ContentViewProject_2: View {
     var body: some View {
 //        LinearGradient(colors: [.red, .green, .blue], startPoint: .top, endPoint: .bottom)
-        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+//        LinearGradient(colors: [.white, .black], startPoint: .top, endPoint: .bottom)
+        
+        // Linear Gradient with stops
+//        LinearGradient(gradient: Gradient(stops: [
+//            Gradient.Stop(color: .white, location: 0.45),
+//            Gradient.Stop(color: .black, location: 0.55)
+//        ]), startPoint: .top, endPoint: .bottom)
+        // OR
+        LinearGradient(gradient: Gradient(stops: [
+            .init(color: .white, location: 0.45),
+            .init(color: .black, location: 0.55)
+        ]), startPoint: .top, endPoint: .bottom)
     }
 }
 
