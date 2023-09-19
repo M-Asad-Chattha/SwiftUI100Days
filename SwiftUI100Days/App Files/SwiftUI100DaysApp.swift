129a48b2968272b125a3ae349803aad770b45ad0
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-09-19 08:33:45 +0500

    Custom Modifiers extended by Views

--- SwiftUI100Days/App Files/SwiftUI100DaysApp.swift
+++ SwiftUI100Days/App Files/SwiftUI100DaysApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI100DaysApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentViewProject_2()
+            ContentViewProject_3()
         }
     }
 }
