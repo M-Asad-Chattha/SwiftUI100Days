7c4340d402fb0f2f41fc0731f9276ec303c24407
Muhammad Asad <muhammad.asad@o9tech.com>
2023-11-14 17:55:07 +0500

    add(WordScramble): complete Day 29

--- SwiftUI100Days/Project/WordScramble.swift
+++ SwiftUI100Days/Project/WordScramble.swift
@@ -1,5 +1,6 @@
 //
 //  ContentViewProject_5.swift
+//  Project #5
 //  SwiftUI100Days
 //
 //  Created by o9tech on 10/11/2023.
@@ -8,8 +9,39 @@
 import SwiftUI
 
 struct WordScramble: View {
+    let people = ["person1", "person2", "person3", "person4", "person5", "person6", "person7", "person8", "person9", "person10"]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            List {
+                Button("Check Spelling") {
+                    spellCheck(word: "Swifto")
+                }
+                
+                Section("Dynamic Data") {
+                    ForEach(people, id: \.self) {
+                        Text("Row \($0)")
+                    }
+                }
+            }
+            .navigationTitle("Word Scramble")
+        }
+    }
+}
+
+// MARK: - Helper Methods
+extension WordScramble {
+    func randomPerson() -> String {
+        people.randomElement() ?? "person1"
+    }
+    
+    func spellCheck(word: String) {
+        let checker = UITextChecker()
+        
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        print(misspelledRange)
+        print(misspelledRange.location == NSNotFound)
     }
 }
 
