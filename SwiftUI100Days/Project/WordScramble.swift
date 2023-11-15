4854faeda1f859e33d6fdf910611f37f1df68405
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-15 23:27:55 +0500

    add: complete Day # 30. Validating words with UITextChecker.

--- SwiftUI100Days/Project/WordScramble.swift
+++ SwiftUI100Days/Project/WordScramble.swift
@@ -13,12 +13,17 @@ struct WordScramble: View {
     @State private var rootWord = ""
     @State private var newWord = ""
     
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+    
     var body: some View {
         NavigationStack {
             List {
                 Section {
                     TextField("Enter your word", text: $newWord)
                         .autocapitalization(.none)
+                        .autocorrectionDisabled()
                 }
                 
                 Section {
@@ -33,6 +38,9 @@ struct WordScramble: View {
             .navigationTitle(rootWord)
             .onSubmit(addNewWord)
             .onAppear(perform: startGame)
+            .alert(alertTitle, isPresented: $showingAlert, actions: {}) {
+                Text(alertMessage)
+            }
         }
     }
 }
@@ -42,6 +50,20 @@ extension WordScramble {
     func addNewWord() {
         let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard answer.count > 0 else { return } // exit if the answer is empty
+        
+        guard isOriginal(word: answer) else {
+            showError(title: "Word used already", message: "Be more original")
+            return
+        }
+        guard  isPossible(word: answer) else {
+            showError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        guard isReal(word: answer) else {
+            showError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
         withAnimation { usedWords.insert(answer, at: 0) }
         newWord = ""
     }
@@ -56,6 +78,36 @@ extension WordScramble {
         }
         
         fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let match = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return match.location == NSNotFound
+    }
+    
+    func showError(title: String, message: String) {
+        alertTitle = title
+        alertMessage = message
+        showingAlert = true
     }
 }
 
