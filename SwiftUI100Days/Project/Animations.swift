9f1e0b272d7549e0e49b79b5458f4d6b93b27039
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-22 21:21:33 +0500

    add: complete Creating implicit animations in Day-32, project-6 part 1

--- SwiftUI100Days/Project/Animations.swift
+++ SwiftUI100Days/Project/Animations.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct Animations: View {
+
+    // MARK: - @State
+    @State private var animationAmount = 1.0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button("Tap me") {
+           animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
     }
 }
 
