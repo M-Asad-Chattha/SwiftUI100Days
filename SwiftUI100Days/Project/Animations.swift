7317d4dbd507c3e5e3e8faf06ca65ab143501e41
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-22 08:17:02 +0500

    add: add new Animations file to start work on Project-6

---
+++ SwiftUI100Days/Project/Animations.swift
@@ -0,0 +1,18 @@
+//
+//  Animations.swift
+//  SwiftUI100Days
+//
+//  Created by Muhammad Asad Chattha on 22/11/2023.
+//
+
+import SwiftUI
+
+struct Animations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Animations()
+}
