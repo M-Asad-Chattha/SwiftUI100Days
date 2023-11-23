0a04694b346e91bf2449fa43044553085d075afb
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-23 22:46:06 +0500

    add: complete "Creating explicit animations" apply animation using binding on Day-32, project-6 part 1

--- SwiftUI100Days/Project/Animations.swift
+++ SwiftUI100Days/Project/Animations.swift
@@ -10,26 +10,23 @@ import SwiftUI
 struct Animations: View {
 
     // MARK: - @State
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
 
     var body: some View {
-        print(animationAmount)
-        
-        return VStack {
-            Stepper("Scale Amount", value: $animationAmount.animation(
-                .easeInOut(duration: 1)
-                .repeatCount(3, autoreverses: true)
-            ), in: 1...10)
-            Spacer()
-            Button("Tap me") {
-                animationAmount += 1
+        Button("Tap me") {
+            withAnimation(.spring(duration: 1, bounce: 0.5)) {
+                animationAmount += 360
+            } completion: {
+                animationAmount = 0.0
             }
-            .padding(50)
-            .background(.red)
-            .foregroundStyle(.white)
-            .clipShape(.circle)
-            .scaleEffect(animationAmount)
         }
+        .padding(50)
+        .background(.red)
+        .foregroundStyle(.white)
+        .clipShape(.circle)
+        .rotation3DEffect(
+            .degrees(animationAmount), axis: (x: 0, y: 1, z: 0)
+        )
     }
 }
 
