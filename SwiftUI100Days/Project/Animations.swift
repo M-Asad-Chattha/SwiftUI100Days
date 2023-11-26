9a6edf8c1e3c3c49f7a28be899ebd7af020333d2
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-26 15:16:08 +0500

    add: complete "Animating gestures" Add delay animation for a snake-like effect with drag gesture.
    Refers to Day-33, project-6 part 2
    Tutorial source: https://www.hackingwithswift.com/books/ios-swiftui/animating-gestures

--- SwiftUI100Days/Project/Animations.swift
+++ SwiftUI100Days/Project/Animations.swift
@@ -8,39 +8,32 @@
 import SwiftUI
 
 struct Animations: View {
+    
+    // MARK: - State
+    @State private var enabled = false
+    @State private var dragAmount = CGSize.zero
+    
+    // MARK: - Properties
+    let letters = Array("Hello, SwiftUI!")
 
+    // MARK: - Body
     var body: some View {
-        Flag()
-    }
-}
-
-/// Draggable Flag with Animation Effect
-struct Flag: View {
-
-    // MARK: - @State
-    @State private var dragAmount: CGSize  = .zero
-
-    var body: some View {
-        LinearGradient(gradient: Gradient(stops: [
-            .init(color: .white, location: 0.2),
-            .init(color: .green, location: 0.2)
-        ]), startPoint: .leading, endPoint: .trailing)
-        .frame(width: 300, height: 200)
-        .clipShape(.rect(cornerRadius: 10))
-
-        .overlay(
-            Image(.icMoonStar)
-                .resizable()
-                .scaledToFit()
-                .scaleEffect(0.7)
-        )
-        .offset(dragAmount)
+        HStack(spacing: 0) {
+            ForEach(0..<letters.count, id: \.self) { number in
+                Text(String(letters[number]))
+                    .padding(5)
+                    .font(.title)
+                    .background(enabled ? .blue : .orange)
+                    .offset(dragAmount)
+                    .animation(.linear.delay(Double(number) / 20), value: dragAmount)
+            }
+        }
         .gesture(
             DragGesture()
-                .onChanged{ dragAmount = $0.translation }
-                .onEnded { _ in
-                    // Reset the drag amount
-                    withAnimation(.bouncy()) { dragAmount = CGSize.zero }
+                .onChanged { dragAmount = $0.translation }
+                .onEnded {_ in
+                    dragAmount = .zero
+                    enabled.toggle()
                 }
         )
     }
