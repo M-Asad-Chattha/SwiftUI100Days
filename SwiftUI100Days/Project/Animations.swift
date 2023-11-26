75a9719494f198c22664636c1bce20cc56c31e0d
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2023-11-26 17:48:30 +0500

    add; complete "Building custom transitions using ViewModifier" Apply transition using custom ViewModifier and AnyTransition extension.
    Refers to Day-33, project-6 part 2
    Tutorial source: https://www.hackingwithswift.com/books/ios-swiftui/building-custom-transitions-using-viewmodifier

--- SwiftUI100Days/Project/Animations.swift
+++ SwiftUI100Days/Project/Animations.swift
@@ -7,27 +7,58 @@
 
 import SwiftUI
 
+struct CornerRotateModifier: ViewModifier {
+    let angle: Double
+    let anchor: UnitPoint
+
+    func body(content: Content) -> some View {
+        content
+            .rotationEffect(.degrees(angle), anchor: anchor)
+            .clipped()
+    }
+}
+
+extension AnyTransition {
+    static var pivot: AnyTransition {
+        .modifier(
+            active: CornerRotateModifier(angle: -90, anchor: .topLeading),
+            identity: CornerRotateModifier(angle: .zero, anchor: .topLeading)
+        )
+    }
+}
+
 struct Animations: View {
-    
+
     // MARK: - State
     @State private var isShowingRed = false
-    
+
     // MARK: - Properties
 
     // MARK: - Body
     var body: some View {
-        VStack {
-            Button("Tap Me") {
-                withAnimation { isShowingRed.toggle() }
-            }
+        ZStack {
+            Rectangle()
+                .fill(.blue)
+                .frame(width: 200, height: 200)
             
             if isShowingRed {
                 Rectangle()
                     .fill(.red)
                     .frame(width: 200, height: 200)
-                    .clipShape(.rect(cornerRadius: 10))
-                    // .transition(.scale) // Scaling from smallest to largest (declraed size)
-                    .transition(.asymmetric(insertion: .scale, removal: .opacity)) // Use one transition when the view is being shown and another when it’s disappearing.
+                     .transition(.pivot) // Defining pivot in AnyTransition extension
+                // Same Behaviour but different Approach
+//                    .transition(
+//                        .modifier(
+//                            active: CornerRotateModifier(angle: -90, anchor: .topLeading),
+//                            identity: CornerRotateModifier(angle: 0, anchor: .topLeading)
+//                        )
+//                    )
+                    
+            }
+        }
+        .onTapGesture {
+            withAnimation {
+                isShowingRed.toggle()
             }
         }
     }
