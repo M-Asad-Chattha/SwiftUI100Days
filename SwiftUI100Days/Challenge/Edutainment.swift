10ac56f57ea0b2717c2ae82adb201531be5f57ee
Shuja Ud Din <shuja@pryvatenow.com>
2024-01-01 18:36:26 +0500

    feat: add UI Controls; Segmented Control, Button

--- SwiftUI100Days/Challenge/Edutainment.swift
+++ SwiftUI100Days/Challenge/Edutainment.swift
@@ -13,17 +13,43 @@ struct Edutainment: View {
     
     // MARK: - Properties
     @State private var numberOfTable = 2
+    @State private var selectedNumberForQuestionsToAsk = 0
+
+    let segments = [5, 10, 20]
 
     var body: some View {
-        Stepper("Table Number: \(numberOfTable)", value: $numberOfTable, in: 2...12)
-        
-        
+        NavigationView {
+            VStack {
+                Stepper("Table Number: \(numberOfTable)", value: $numberOfTable, in: 2...12)
+                    .padding()
+                Picker("Number of Questions", selection: $selectedNumberForQuestionsToAsk) {
+                    ForEach(segments.indices, id: \.self) { index in
+                        Text("\(segments[index])")
+                    }
+                }
+                .pickerStyle(.segmented)
+                
+                Spacer()
+                Button("Start Game", action: startGame)
+                .buttonStyle(.borderedProminent)
+            }
+            .padding()
+            .navigationTitle("Edutainment")
+        }
     }
 }
 
 // MARK: - Enums
 extension Edutainment {
   
+}
+
+// MARK: - Helper Methods
+extension Edutainment {
+  
+    func startGame() {
+        
+    }
 }
 
 #Preview {
