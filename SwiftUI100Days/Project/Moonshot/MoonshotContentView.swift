1744571e4aa800f98815275107cf1344980c4474
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-01-21 22:44:05 +0500

    feat: setup Moonshot (project-8)
    refers to: https://www.hackingwithswift.com/books/ios-swiftui/moonshot-introduction

---
+++ SwiftUI100Days/Project/Moonshot/MoonshotContentView.swift
@@ -0,0 +1,18 @@
+//
+//  MoonshotContentView.swift
+//  SwiftUI100Days
+//
+//  Created by Muhammad Asad Chattha on 21/01/2024.
+//
+
+import SwiftUI
+
+struct MoonshotContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MoonshotContentView()
+}
