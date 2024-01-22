4061ab81b4f1ea84a0b7aa3918f2270e502e1e40
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-01-22 23:20:50 +0500

    feat: work with scrolling data using ScrollView
    refers to https://www.hackingwithswift.com/books/ios-swiftui/how-scrollview-lets-us-work-with-scrolling-data

--- SwiftUI100Days/Project/Moonshot/MoonshotContentView.swift
+++ SwiftUI100Days/Project/Moonshot/MoonshotContentView.swift
@@ -7,15 +7,31 @@
 
 import SwiftUI
 
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        print("Creating a new CustomTextx")
+        self.text = text
+    }
+}
+
 struct MoonshotContentView: View {
 
     var body: some View {
-        Image(.example)
-            .resizable()
-            .scaledToFit()
-            .containerRelativeFrame(.horizontal) { size, axis in
-                size * 0.8
+        ScrollView(.vertical) {
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText(text: "Item \($0)")
+                        .font(.title)
+                }
             }
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
