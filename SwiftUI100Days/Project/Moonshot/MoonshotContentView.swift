bc1bc6c65f33e0752e5470bec83a95a0d74817ab
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-01-22 22:44:44 +0500

    feat: resizing images to fit the available space according to screen
    refers to https://www.hackingwithswift.com/books/ios-swiftui/resizing-images-to-fit-the-available-space

--- SwiftUI100Days/Project/Moonshot/MoonshotContentView.swift
+++ SwiftUI100Days/Project/Moonshot/MoonshotContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct MoonshotContentView: View {
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(.example)
+            .resizable()
+            .scaledToFit()
+            .containerRelativeFrame(.horizontal) { size, axis in
+                size * 0.8
+            }
     }
 }
 
