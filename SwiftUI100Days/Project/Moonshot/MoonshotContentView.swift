3eb1e3c0fd204852d1881354e31c298bd2e62711
Muhammad Asad Chattha <m.asad.chatthaa@gmail.com>
2024-01-25 08:33:26 +0500

    feat: add Color extension, decode date, add Vertical Grid to show mission detail
    refers to  https://www.hackingwithswift.com/books/ios-swiftui/formatting-our-mission-view

--- SwiftUI100Days/Project/Moonshot/MoonshotContentView.swift
+++ SwiftUI100Days/Project/Moonshot/MoonshotContentView.swift
@@ -9,10 +9,33 @@ import SwiftUI
 
 struct MoonshotContentView: View {
     let astronauts: [String: Astronaut] = Bundle.main.load("astronauts.json")
-    let mission: [Mission] = Bundle.main.load("missions.json")
+    let missions: [Mission] = Bundle.main.load("missions.json")
+    
+    let columns = [GridItem(.adaptive(minimum: 150))]
     
     var body: some View {
-        Text(String(mission.count))
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        VStack {
+                            Image(mission.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(maxWidth: 100, maxHeight: 100)
+                            
+                            VStack {
+                                Text(mission.displayName)
+                                    .font(.headline)
+                                Text(mission.formattedLaunchDate)
+                                    .font(.caption)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
